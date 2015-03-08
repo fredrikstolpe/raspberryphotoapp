cb69015b205b5da941a5cdd84f8d5e8d643d64d6
Fredrik Stolpe <fredrik.stolpe@gmail.com>
2015-03-08 23:05:12 +0100

    Take photo, get latest photos, gpio pins

--- node/app.js
+++ node/app.js
@@ -1,29 +1,47 @@
 var express = require("express");
 var config = require("configure");
-var raspicam = require("raspicam");
 var cameraService = require("./cameraservice.js");
+var fs = require("fs");
+var gpio = require("node-gpio");
 
 var app = express();
 
-app.use(function(){
-    app.use(express.static(__dirname + '/images'));
-});
-
-app.get('/api', function (req, res) {
-  res.send('API is running');
-});
+app.use("/" + config.imageFolder, express.static(__dirname + "/" + config.imageFolder));
 
 app.get('/takephoto', function (req, res) {
+	console.log("takephoto");
 	cameraService.takePhoto()
 	.then(
 		function(value){
-			res.send('Done');
+			console.log(value);
+			res.send(value);
 		},
 		function(error){
+			console.log("hej");
 			res.send('Failed');
 		}
 	)
 });
+
+app.get("/latestphotos", function (req, res){
+	fs.readdir(config.imageFolder, function(err, files){
+		files.sort();
+		files.reverse();
+		files = files.slice(0, 20);
+		files = files.map(function(file){
+			return "/" + config.imageFolder + "/" + file;
+		});
+		res.json(files);
+	});
+});
+
+var button = new gpio.GPIO(config.pins.button);
+button.open();
+button.setMode(gpio.IN);
+button.on("changed", function (value) {
+	console.log(value);
+});
+button.listen();
 
 app.listen(80);
 console.log("Up and running");
--- node/cameraservice.js
+++ node/cameraservice.js
@@ -1,9 +1,23 @@
-var q = require('q');
+var config = require("configure");
+var q = require("q");
+var moment = require("moment");
+var child_process = require("child_process");
 
 module.exports = {
 	takePhoto : function(){
 		var deferred = q.defer();
-		deferred.resolve();
-		return deferred;
+		var filename = moment().format("YYYYMMDDHHmmss-SSS.jpg");
+		var path = config.imageFolder + "/" + filename;
+		child_process.exec("raspistill -w " + config.image.width + "  -h " + config.image.height + " -q " + config.image.quality + " -o '" + path + "'", function(error, stdout, stderr){
+			console.log('Child Process STDOUT: '+stdout);
+			console.log('Child Process STDERR: '+stderr);
+			if (error){
+				deferred.reject(error);
+			}
+			else{
+				deferred.resolve(filename);
+			}
+		})
+		return deferred.promise;
 	}
 }
